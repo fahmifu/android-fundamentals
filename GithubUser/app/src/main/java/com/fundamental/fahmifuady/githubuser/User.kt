9a6a154ce67ef33a9372548881838cdb75cf4822
fahmifu <husni.fuad22@gmail.com>
2020-10-29 23:01:08 +0700

    define a "data class User"

---
+++ GithubUser/app/src/main/java/com/fundamental/fahmifuady/githubuser/User.kt
@@ -0,0 +1,13 @@
+package com.fundamental.fahmifuady.githubuser
+
+data class User (
+    var username: String,
+    var name: String,
+    var location: String,
+    var repository: String,
+    var companiy: String,
+    var followers: String,
+    var following: String,
+    var photo: Int
+
+)
