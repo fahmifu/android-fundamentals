4329d71192fb26f1514733023f16d22c2dd60be5
fahmifu <husni.fuad22@gmail.com>
2020-11-01 14:53:23 +0700

    implementing a RecyclerView

--- GithubUser/app/src/main/java/com/fundamental/fahmifuady/githubuser/UserAdapter.kt
+++ GithubUser/app/src/main/java/com/fundamental/fahmifuady/githubuser/UserAdapter.kt
@@ -1,11 +1,9 @@
 package com.fundamental.fahmifuady.githubuser
 
-import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
