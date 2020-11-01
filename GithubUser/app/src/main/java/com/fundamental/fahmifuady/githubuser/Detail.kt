1e77c1bfe55287b133b9d8857310a7a1a66850c4
fahmifu <husni.fuad22@gmail.com>
2020-11-01 15:59:55 +0700

    make button sign up working...

--- GithubUser/app/src/main/java/com/fundamental/fahmifuady/githubuser/Detail.kt
+++ GithubUser/app/src/main/java/com/fundamental/fahmifuady/githubuser/Detail.kt
@@ -1,13 +1,17 @@
 package com.fundamental.fahmifuady.githubuser
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import de.hdodenhof.circleimageview.CircleImageView
 
-class Detail: AppCompatActivity() {
+class Detail: AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val EXTRA_NAME = "extra_name"
@@ -38,6 +42,7 @@ class Detail: AppCompatActivity() {
         val repository1: TextView = findViewById(R.id.txt_number_of_repository1)
         val location: TextView = findViewById(R.id.txt_location)
         val company: TextView = findViewById(R.id.txt_company)
+        val signup: Button = findViewById(R.id.btn_signup)
 
         val vAvatar = intent.getIntExtra(EXTRA_AVATAR, 0)
         val vName= intent.getStringExtra(EXTRA_NAME)
@@ -62,8 +67,19 @@ class Detail: AppCompatActivity() {
             .load(vAvatar)
             .apply(RequestOptions())
             .into(avatar)
+
+        signup.setOnClickListener(this)
     }
 
+    override fun onClick(v: View?){
+        when(v?.id) {
+            R.id.btn_signup -> {
+                val Url = "https://github.com/join"
+                val moveIntent = Intent(Intent.ACTION_VIEW, Uri.parse("$Url"))
+                startActivity(moveIntent)
+            }
+        }
+    }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
