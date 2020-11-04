07cf6f1bcb5beb3fa5bc8b1d374cc64aa7e88309
fahmifu <husni.fuad22@gmail.com>
2020-11-04 10:27:01 +0700

    sent data with bundle process

--- MyNavigation/app/src/main/java/com/fundamental/fahmifuady/mynavigation/CategoryFragment.kt
+++ MyNavigation/app/src/main/java/com/fundamental/fahmifuady/mynavigation/CategoryFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_category.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,5 +57,19 @@ class CategoryFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+        // add val
+        val EXTRA_NAME = "extra_name"
+        val EXTRA_STOCK = "extra_stock"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_category_lifestyle.setOnClickListener { view ->
+            val mBundle = Bundle()
+            mBundle.putString(EXTRA_NAME, "Lifestyle")
+            mBundle.putLong(EXTRA_STOCK, 7)
+            view.findNavController()
+                .navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
+        }
     }
 }
