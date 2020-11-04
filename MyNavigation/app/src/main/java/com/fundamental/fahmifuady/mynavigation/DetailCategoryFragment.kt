f287b1617124796ecf305e15c5d408557247ac61
fahmifu <husni.fuad22@gmail.com>
2020-11-04 10:01:55 +0700

    adding a detail category fragment

--- MyNavigation/app/src/main/java/com/fundamental/fahmifuady/mynavigation/DetailCategoryFragment.kt
+++ MyNavigation/app/src/main/java/com/fundamental/fahmifuady/mynavigation/DetailCategoryFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [DetailCategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class DetailCategoryFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,7 +34,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
     }
 
     companion object {
@@ -47,26 +44,16 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment DetailCategoryFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            DetailCategoryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        btn_category.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
-        )
-        btn_profile.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
-        }
     }
 }
