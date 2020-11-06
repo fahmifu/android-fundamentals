3e7498155e9cfd294784acb1d36ea4e089471fd8
fahmifu <husni.fuad22@gmail.com>
2020-11-06 07:41:43 +0700

    adding a search view

--- MyActionBar/app/src/main/java/com/fundamental/fahmifuady/myactionbar/MainActivity.kt
+++ MyActionBar/app/src/main/java/com/fundamental/fahmifuady/myactionbar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.fundamental.fahmifuady.myactionbar
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            // use this method when the search is complete or OK
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            //Use this method to respond to each letter change in the searchView
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
         return true
     }
 
